1e7d3655ab37bf6d93a14365e2864dcde70b3a26
presidentofkazakhstan <alibekbekhan@gmail.com>
2019-03-29 13:38:38 +0600

    first commit

---
+++ Man.cs
@@ -0,0 +1,9 @@
+﻿namespace module15_homework
+{
+    public class Man
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module15_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первое задание");
+            Type console = typeof(Console);
+            MethodInfo[] methods = console.GetMethods();
+            Console.WriteLine("Методы класса Console: ");
+            foreach (MethodInfo methodinfo in methods)
+            {
+                Console.Write(methodinfo.ReturnType.Name + "\t" + methodinfo.Name);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Второе задание");
+
+            Man man = new Man()
+            {
+                Name = "Muratzhan",
+                Age = 16,
+                City = "Nursultan"
+            };
+            Type type = typeof(Man);
+            Console.WriteLine("Тип: " + type.Name);
+            PropertyInfo[] properties = type.GetProperties();
+            Console.WriteLine("Методы: ");
+            foreach (var property in properties)
+            {
+                Console.WriteLine(property.PropertyType + ":\t" + property.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
